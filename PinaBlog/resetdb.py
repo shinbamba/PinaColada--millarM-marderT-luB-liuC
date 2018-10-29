a8129aee05a8646172583e194d09eb9a09e74959
Claire Liu <cliu5@stuy.edu>
2018-10-29 01:10:16 -0400

    Update resetdb.py

--- PinaBlog/resetdb.py
+++ PinaBlog/resetdb.py
@@ -17,9 +17,9 @@ def resetTable():
         c.execute("INSERT INTO login VALUES('john', 'doe')")
         c.execute("INSERT INTO login VALUES('jane', 'doe')")
         c.execute("INSERT INTO blog VALUES('john', 'Sports', 'Test Blog', 'TEST BLOG', 1)")
-        c.execute("INSERT INTO blog VALUES('jane', 'Other', 'Other Test Blog', 'MEST BLOG', 1)")
+        c.execute("INSERT INTO blog VALUES('jane', 'Other', 'Other Test Blog', 'RANDOM BLOG', 1)")
         c.execute("INSERT INTO post VALUES('john', 'Test Post', 'TEST POST', 1, 1, CURRENT_TIMESTAMP)")
-        c.execute("INSERT INTO post VALUES('jane', 'Other Test Post', 'MEST POST', 1, 1, CURRENT_TIMESTAMP)")
+        c.execute("INSERT INTO post VALUES('jane', 'Other Test Post', 'RANDOM POST', 1, 1, CURRENT_TIMESTAMP)")
     except:
         c.execute("CREATE TABLE login(username TEXT, password TEXT)")
         c.execute("CREATE TABLE blog(username TEXT, category TEXT, blog_title TEXT, description TEXT, blog_id INTEGER)")
@@ -27,9 +27,9 @@ def resetTable():
         c.execute("INSERT INTO login VALUES('john', 'doe')")
         c.execute("INSERT INTO login VALUES('jane', 'doe')")
         c.execute("INSERT INTO blog VALUES('john', 'Sports', 'Test Blog', 'TEST BLOG', 1)")
-        c.execute("INSERT INTO blog VALUES('jane', 'Other', 'Other Test Blog', 'MEST BLOG', 1)")
+        c.execute("INSERT INTO blog VALUES('jane', 'Other', 'Other Test Blog', 'RANDOM BLOG', 1)")
         c.execute("INSERT INTO post VALUES('john', 'Test Post', 'TEST POST', 1, 1, CURRENT_TIMESTAMP)")
-        c.execute("INSERT INTO post VALUES('jane', 'Other Test Post', 'MEST POST', 1, 1, CURRENT_TIMESTAMP)")
+        c.execute("INSERT INTO post VALUES('jane', 'Other Test Post', 'RANDOM POST', 1, 1, CURRENT_TIMESTAMP)")
         
 def readFrom():
     c.execute("SELECT * FROM login")
